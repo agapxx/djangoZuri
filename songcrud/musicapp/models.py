50a3ffee938b30d598ef41a42db97faf04ff3730
agapxx <okontaagape@gmail.com>
2022-10-29 03:57:04 +0100

    done

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=400)
+    date_released = models.DateField()
+    likes = models.CharField(max_length=400)
+
+
+class Lyric(models.Model):
+    song_id = models.ForeignKey(to=Song, on_delete=Song)
+    content = models.CharField(max_length=200000000)
+
+
+
